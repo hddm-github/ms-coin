ec6906074bd5b17a84188039be00ee8d1c618223
hddm-github <abel_liq@163.com>
2026-06-03 21:30:15 +0800

    feat: 初始化项目，实现用户中心服务基础骨架及手机注册接口
    - 初始化 Go 工作区（go.work）及项目基础骨架
    - 引入 grpc-common 模块，存放 Protobuf 协议及生成的 gRPC 客户端/服务端代码
    - 实现 ucenter gRPC 微服务，定义 RegisterByPhone 接口逻辑骨架
    - 实现 ucenter-api HTTP 服务，暴露 /uc/register/phone 接口并调用 ucenter RPC 接口
    - 配置 Etcd 服务发现与注册

---
+++ ucenter-api/internal/logic/register_logic.go
@@ -0,0 +1,35 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.10.1
+
+package logic
+
+import (
+	"context"
+	"grpc-common/ucenter/types/register"
+	"time"
+	"ucenter-api/internal/svc"
+	"ucenter-api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *RegisterLogic) Register(req *types.Request) (resp *types.Response, err error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	l.svcCtx.URegisterRpc.RegisterByPhone(ctx, &register.RegReq{})
+	return
+}
